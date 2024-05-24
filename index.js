f7820938a52995c0e92c3b0f92643f1c6a867e56
yanniao0827 <yanning0113niao@gmail.com>
2024-05-24 09:34:01 +0800

    新增404介面

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ app.get('/',(req,res)=>{
     res.send(`<h2>晚安</h2>`)
 });
 
-// 如果有設定就是3002，沒有ˋ就是3001
+// 設定404介面，一定要放在路由之後，放在路由前面會直接判定是404狀態，就不會再往後判斷
+app.use((req,res)=>{
+    res.type("text/plain");
+    res.status(404);
+    res.send("餔餔");
+})
+
+// 如果有設定就是3002，沒有就是3001
 const port =process.env.WEB_PORT || 3002;
 app.listen(port,()=>{
     console.log(`Server start:port ${port}`)
