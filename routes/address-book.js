600780acef4222d2c5e6026b9621ef68b92a05ff
Leana <yanning0113niao@gmail.com>
2024-05-28 23:47:38 +0800

    顯示分頁資料

--- routes/address-book.js
+++ routes/address-book.js
@@ -4,9 +4,26 @@ import db from "./../utils/connect-mysql.js";
 const router =express.Router();
 
 router.get("/",async(req,res)=>{
+    let success=false;
+    const perPage=25; //每頁最多25筆
+    let page=parseInt(req.query.page)||1; //由query string取得page的值
+    // 頁數<1就跳轉到第一頁 
+    if(page<1){
+        return res.redirect("?page=1");
+    }
+
     const t_sql="SELECT COUNT(1) AS totalRows FROM address_book";
-    const [results]=await db.query(t_sql);
-    res.json(results);
+    const [[{totalRows}]] =await db.query(t_sql);
+    let totalPages=0; //總頁數預設值為0
+    let rows=[];
+    // 取得分頁資料
+    if(totalRows){
+        totalPages=Math.ceil(totalRows / perPage);
+        const sql=`SELECT * FROM \`address_book\`LIMIT ${(page-1)*perPage},${perPage}`;
+        
+        [rows]=await db.query(sql);
+        }
+    res.json({success,perPage,page,totalRows,rows});
 });
 
 
