82f944550ea0e24ff046c077a36c5dfe4278c637
Leana <yanning0113niao@gmail.com>
2024-05-28 23:10:44 +0800

    顯示共有幾筆資料

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,9 +1,12 @@
 import express from "express";
+import db from "./../utils/connect-mysql.js";
 // 建立路由器，在router身上定義方法
 const router =express.Router();
 
-router.get("/",(req,res)=>{
-    res.json(req.originalUrl);
+router.get("/",async(req,res)=>{
+    const t_sql="SELECT COUNT(1) AS totalRows FROM address_book";
+    const [results]=await db.query(t_sql);
+    res.json(results);
 });
 
 
