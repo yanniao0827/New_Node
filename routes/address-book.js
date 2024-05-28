e495ffa6992b7d79f5cd393612a3695e9b87ef0b
Leana <yanning0113niao@gmail.com>
2024-05-29 00:45:28 +0800

    分頁按鈕

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,39 +1,64 @@
 import express from "express";
 import moment from "moment-timezone";
 import db from "./../utils/connect-mysql.js";
-// 建立路由器，在router身上定義方法
-const router =express.Router();
-const dateFormat="YYYY-MM-DD"
 
-router.get("/",async(req,res)=>{
-    let success=false;
-    const perPage=25; //每頁最多25筆
-    let page=parseInt(req.query.page)||1; //由query string取得page的值
-    // 頁數<1就跳轉到第一頁 
-    if(page<1){
-        return res.redirect("?page=1");
+const dateFormat = "YYYY-MM-DD";
+const router = express.Router();
+
+const getListData = async (req) => {
+  let success = false;
+  let redirect = "";
+
+  const perPage = 25; // 每頁最多有幾筆資料
+  let page = parseInt(req.query.page) || 1; // 從 query string 最得 page 的值
+  if (page < 1) {
+    redirect = "?page=1";
+    return { success, redirect };
+  }
+
+  const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
+  const [[{ totalRows }]] = await db.query(t_sql);
+  let totalPages = 0; // 總頁數, 預設值
+  let rows = []; // 分頁資料
+  if (totalRows) {
+    totalPages = Math.ceil(totalRows / perPage);
+    if (page > totalPages) {
+      redirect = `?page=${totalPages}`;
+      return { success, redirect };
     }
-
-    const t_sql="SELECT COUNT(1) AS totalRows FROM address_book";
-    const [[{totalRows}]] =await db.query(t_sql);
-    let totalPages=0; //總頁數預設值為0
-    let rows=[];
     // 取得分頁資料
-    if(totalRows){
-        totalPages=Math.ceil(totalRows / perPage);
-        if(page>totalPages){
-            return res.redirect(`?page=${totalPages}`);
-        }
-        const sql=`SELECT * FROM \`address_book\`LIMIT ${(page-1)*perPage},${perPage}`;
-        
-        [rows]=await db.query(sql);
-        rows.forEach((el)=>{
-            el.birthday=moment(el.birthday).format(dateFormat)
-        })
-        }
-    // res.json({success,perPage,page,totalRows,rows});
-    res.render("address-book/list",{success,perPage,page,totalRows,rows});
+    const sql = `SELECT * FROM \`address_book\` LIMIT ${
+      (page - 1) * perPage
+    },${perPage}`;
+
+    [rows] = await db.query(sql);
+    rows.forEach((el) => {
+      el.birthday = moment(el.birthday).format(dateFormat);
+    });
+  }
+  success = true;
+  return {
+    success,
+    perPage,
+    page,
+    totalRows,
+    totalPages,
+    rows,
+  };
+};
+
+router.get("/", async (req, res) => {
+  const data = await getListData(req);
+  if (data.redirect) {
+    return res.redirect(data.redirect);
+  }
+  if (data.success) {
+    res.render("address-book/list", data);
+  }
 });
 
-
+router.get("/api", async (req, res) => {
+  const data = await getListData(req);
+  res.json(data);
+});
 export default router;
