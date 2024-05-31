12f2d6fa509ccc939d7587a18eefe4265bbe77f5
yanniao0827 <yanning0113niao@gmail.com>
2024-05-31 14:41:41 +0800

    cookie 的 cross domain 問題

--- index.js
+++ index.js
@@ -24,7 +24,14 @@ app.use(express.urlencoded({extended: true}));
 // 只會解析 application/json
 app.use(express.json());
 
-app.use(cors());
+const corsOptions={
+  credentials:true,
+  origin:(origin,cb)=>{
+    console.log({origin});
+    cb(null,true);
+  }
+}
+app.use(cors(corsOptions));
 
 app.use(session({
   saveUninitialized:false,
